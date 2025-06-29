608c2f2d00debe1603bdddf1f2fc06d6e03ab0a1
Thorsten Bylicki <109308073+bylickilabs@users.noreply.github.com>
2025-06-29 21:54:45 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,3 +3,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+st.title("StatMaster: Interaktive Data Science Analyse")
+
+# Daten-Upload
+uploaded_file = st.file_uploader("CSV/Excel Datei hochladen", type=["csv", "xlsx"])
+if uploaded_file:
+    if uploaded_file.name.endswith('.csv'):
+        df = pd.read_csv(uploaded_file)
+    else:
+        df = pd.read_excel(uploaded_file)
+    st.write("**Datenvorschau:**")
+    st.dataframe(df)
